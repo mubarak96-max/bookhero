4a6ee0ab0b7cf9188a42915d17d9873c2b3acbd7
Kng Jefas <58780463+NKANGIJAFARI@users.noreply.github.com>
2021-11-08 18:53:51 +0400

    Initial commit

---
+++ pages/api/authaws/index.js
@@ -0,0 +1,19 @@
+import nc from 'next-connect';
+import connectDB from '../../../Backend/config/db';
+import { generateUploadURL } from '../../../Backend/s3';
+
+const handler = nc();
+
+handler.get(async (req, res) => {
+  try {
+    const url = await generateUploadURL();
+
+    return res.send({ url });
+  } catch (error) {
+    return res.status(500).json({ error: error.message });
+  }
+});
+
+// Check if name, email or password is provided
+
+export default connectDB(handler);
