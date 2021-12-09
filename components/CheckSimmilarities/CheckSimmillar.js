2a093ae4cd0ad6070ace06213275edda5f525e8d
nkangi jafari <nkangijafari@gmail.com>
2021-12-09 18:17:01 +0400

    Simmilars now downloadable

--- components/CheckSimmilarities/CheckSimmillar.js
+++ components/CheckSimmilarities/CheckSimmillar.js
@@ -1,14 +1,20 @@
 //React component that that has an input field for files and a button to upload the file.
 import Papa from 'papaparse';
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
+import arrayToCSV from '../Functions/arrayToCSV';
 import findDuplicates from '../Functions/findDuplicates';
+// import { LoadingButton } from '@mui/lab';
+// import { SaveIcon } from '@mui/icons-material';
 
 const Upload = (props) => {
   const [files, setFiles] = useState('');
+  const [downloadSource, setDownloadSource] = useState('');
 
   const onChange = (e) => {
     setFiles(e.target.files);
   };
+
+  const [loading, setLoading] = React.useState(false);
 
   const csvToJson = async (fileToConvert, fileName) => {
     const parseFile = () => {
@@ -32,6 +38,7 @@ const Upload = (props) => {
 
   const onSubmit = async (e) => {
     e.preventDefault();
+    setLoading(true);
 
     let completeConvertedArray = [];
 
@@ -48,7 +55,17 @@ const Upload = (props) => {
     const finalArray = [].concat.apply([], completeConvertedArray);
 
     const final = findDuplicates(finalArray);
-    console.log('final', final);
+
+    const url = arrayToCSV(final, 'duplicates.csv');
+    console.log(url);
+
+    setDownloadSource(url);
+    setLoading(false);
+
+    // a BUTTON to download the file
+    // const downloadButton = document.createElement('a');
+    // downloadButton.setAttribute('href', url);
+    // downloadButton.setAttribute('download', 'duplicates.csv');
   };
 
   return (
@@ -57,6 +74,28 @@ const Upload = (props) => {
         <input type='file' multiple onChange={onChange} />
         <button type='submit'>Upload</button>
       </div>
+
+      {downloadSource && (
+        <div
+          style={{
+            marginTop: '60px',
+          }}>
+          <a
+            href={downloadSource}
+            download
+            style={{
+              backgroundColor: 'DodgerBlue',
+              border: 'none',
+              color: 'white',
+              padding: '12px 30px',
+              cursor: 'pointer',
+              fontSize: '20px',
+              marginTop: '60px',
+            }}>
+            Download
+          </a>
+        </div>
+      )}
     </form>
   );
 };
