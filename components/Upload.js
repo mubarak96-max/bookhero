31c95da81c4decb4b17b502675756aa6a7529871
nkangi jafari <nkangijafari@gmail.com>
2021-12-15 17:59:12 +0400

    Debug

--- components/Upload.js
+++ components/Upload.js
@@ -114,7 +114,7 @@ const Upload = ({ getUploadedBookInfo, getBlob }) => {
         {isDragActive ? (
           <p>Drop the files here ...</p>
         ) : (
-          <p>Drag 'n' drop some files here, or click to select files</p>
+          <p>Drag and drop some files here, or click to select files</p>
         )}
       </div>
       <br />
