dbecdb6d3dc4e56b9a520259c84da67441478324
unknown <nkangijafari@gmail.com>
2022-02-13 09:15:42 -0800

    Upload while searching ocuurances

--- components/Functions/findDuplicates.js
+++ components/Functions/findDuplicates.js
@@ -11,7 +11,7 @@ const findDuplicates = (arr) => {
   }
 
   for (var key in obj) {
-    if (obj[key].length > 1) {
+    if (obj[key].length > 1 && key !== '') {
       result.push({
         Title: key,
         locations: obj[key],
@@ -20,6 +20,24 @@ const findDuplicates = (arr) => {
   }
 
   console.log(result);
+  const toFindDuplicates = (array) =>
+    array.filter((item, index) => arr.indexOf(item) !== index);
+
+  //A function to get how many times an element occurs in an array
+  const getOccurrence = (array, element) => {
+    return array.reduce((a, e) => (e === element ? a + 1 : a), 0);
+  };
+
+  for (let i = 0; i < result.length; i++) {
+    for (let j = 0; j < result[i].locations.length; j++) {
+      const duplicateElements = getOccurrence(
+        result[i].locations,
+        result[i].locations[j],
+      );
+      console.log(`Duplicates in ${result[i].locations[j]}`, duplicateElements);
+    }
+  }
+
   return result;
 };
 
